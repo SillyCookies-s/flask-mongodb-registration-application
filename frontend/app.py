5ef7d980a5d61c12c80004ef51ba73ddd91f1e89
Naman-UbU <namansondhiya17@gmail.com>
2025-08-11 15:46:39 +0530

    Added OS

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
-BACKEND_URL = 'http://localhost:3000'
+BACKEND_URL = os.getenv('BACKEND_URL', 'http://0.0.0.0:3000')
 
 @app.route('/')
 def home():
