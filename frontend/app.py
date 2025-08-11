db694aba6946ff1dd9ea646ccb5c104c688fd857
Naman-UbU <namansondhiya17@gmail.com>
2025-08-11 16:00:25 +0530

    valid url v1

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-BACKEND_URL = os.getenv('BACKEND_URL', 'http://0.0.0.0:3000')
+BACKEND_URL = os.getenv('BACKEND_URL', 'http://localhost:3000')
 
 @app.route('/')
 def home():
